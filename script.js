29a6ace2546d54a2611b5ae3df4beb11ab121524
sjofrem <sebastian.jofrem@usm.cl>
2021-10-01 19:59:15 -0300

    Added the function playRound

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ function computerPlay(){
     return play[Math.floor(Math.random() * play.length)];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "It's a tie!";
+    }
+    else if((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper")||(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock")||(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors")){
+        return "You lose!";
+    }
+    else{
+        return "You Win!"
+    }
+
+}
+
+console.log(playRound("rock","paper"));

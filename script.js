eb0554c40e6bf1dac2dec88a2b02911600528aad
sjofrem <sebastian.jofrem@usm.cl>
2021-10-07 02:09:55 -0300

    added buttons, styling and change the functions to work with the ui

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
 function computerPlay(){
     let play = ["Rock","Paper","Scissors"];
     return play[Math.floor(Math.random() * play.length)];
 }
 
 function playRound(playerSelection, computerSelection){
+    const results = document.getElementById("results");
     if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return 0;
+        results.innerText = `It's a Tie`;
     }
     else if((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper")||
             (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock")||
             (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors")){
-        return -1;
+        results.innerText = `You Win!`;
+        playerPoints++;
     }
     else{
-        return 1;
+        results.innerText = `You Lose!`;
+        computerPoints++;
     }
+    updatePoints();
 
 }
 
-function game(){
-    let points = 0;
-    let playerSelection;
-    let result;
+function updatePoints(){
+    const player = document.getElementById("player");
+    const cpu = document.getElementById("cpu");
 
+    player.innerText = playerPoints;
+    cpu.innerText = computerPoints;
 
-    for(let i = 0; i< 5; i++){
-        playerSelection = window.prompt("Select rock, paper or scissor (round "+(i+1)+"): ");
-        computerSelection = computerPlay();
-        result = playRound(playerSelection,computerPlay());
-        points += result;
-
-        if(result === 1){
-            console.log("You win the round!")
-        }
-        else if(result === -1){
-            console.log("You lose the round!")
-        }
-        else{
-            console.log("You tied the round!")
-        }
-    }
-    if(points>0){
-        console.log("You won the game!")
-    }
-    else if(points<0){
-        console.log("You lose the game!")
-    }
-    else{
-        console.log("You tied the game!")
-    }
 }
 
 
-game();
+const rockBtn = document.querySelector('#Rock');
+const paperBtn = document.querySelector('#Paper');
+const scissorsBtn = document.querySelector('#Scissors');
+
+rockBtn.addEventListener('click', () => playRound("rock", computerPlay()));
+paperBtn.addEventListener('click', () => playRound("paper", computerPlay()));
+scissorsBtn.addEventListener('click', () => playRound("scissors", computerPlay()));

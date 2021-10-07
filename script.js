f1b1593e0640dd4001cad4227f59ded79b675545
sjofrem <sebastian.jofrem@usm.cl>
2021-10-07 20:53:00 -0300

    added a div that shows the move of the computer and the move of the user

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ let playerPoints = 0;
 let computerPoints = 0;
 
 function computerPlay(){
-    let play = ["Rock","Paper","Scissors"];
+    let play = ["rock","paper","scissors"];
     return play[Math.floor(Math.random() * play.length)];
 }
 
 function playRound(playerSelection, computerSelection){
     const results = document.getElementById("results");
+    const img_player = document.getElementById("box_player");
+    const img_cpu = document.getElementById("box_cpu");
     if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        img_player.src=`./images/${playerSelection}.png`;
+        img_cpu.src=`./images/${computerSelection}.png`;
         results.innerText = `It's a Tie`;
     }
     else if((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper")||
             (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock")||
             (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors")){
+        img_player.src=`./images/${playerSelection}.png`;
+        img_cpu.src=`./images/${computerSelection}.png`;
         results.innerText = `You Win!`;
         playerPoints++;
     }
     else{
+        img_player.src=`./images/${playerSelection}.png`;
+        img_cpu.src=`./images/${computerSelection}.png`;
         results.innerText = `You Lose!`;
         computerPoints++;
     }
@@ -26,11 +34,11 @@ function playRound(playerSelection, computerSelection){
 }
 
 function updatePoints(){
-    const player = document.getElementById("player");
-    const cpu = document.getElementById("cpu");
+    const player = document.getElementById("player_score");
+    const cpu = document.getElementById("cpu_score");
 
-    player.innerText = playerPoints;
-    cpu.innerText = computerPoints;
+    player.innerText = "Player: "+playerPoints;
+    cpu.innerText = "Computer: "+computerPoints;
 
 }
 
